d44cc191aeb48208fac2d97bec74d6d8e605381a
Iniuta <innazolotkomoe@gmail.com>
2023-01-02 21:09:11 +0100

    added revers-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let minus = str.slice(1).split('').reverse().join('');
+    if(n < 0){
+        return +minus
+    } else{g
+        return +(n.toString().split('').reverse().join(''))
+    }
 }
